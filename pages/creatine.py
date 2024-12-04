bdc935581776dd7c332a9c03b761d9237d0257bc
Risgr8 <r2gupta@ucsd.edu>
2024-12-04 15:24:04 -0800

    add feature to creatine

--- pages/creatine.py
+++ pages/creatine.py
@@ -10,17 +10,24 @@ init_creatine_amount = config["creatine"]["amount"]
 creatine_date_refreshed = config["creatine"]["date_refreshed"]
 days_to_refresh = (datetime.date.today() - creatine_date_refreshed).days
 creatine_amount = max(0, init_creatine_amount-(5*days_to_refresh))
+if creatine_date_refreshed != datetime.date.today():
+    config["creatine"]["added_today"] = 0
+
 
 if st.button("add 5g"):
     creatine_amount = min(160, creatine_amount+5)
     config["creatine"]["amount"] = creatine_amount
     config["creatine"]["date_refreshed"] = datetime.date.today()
+    config["creatine"]["added_today"] += 5
+    
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file)
 if st.button("remove 5g"):
     creatine_amount = max(0, creatine_amount-5)
     config["creatine"]["amount"] = creatine_amount
     config["creatine"]["date_refreshed"] = datetime.date.today()
+    config["creatine"]["added_today"] -= 5
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file)
-st.write("current creatine amount: " + str(creatine_amount))
+st.write("current creatine amount: " + str(creatine_amount) + " g")
+st.write("creatine taken today: " + str(config["creatine"]["added_today"]) + " g") 
