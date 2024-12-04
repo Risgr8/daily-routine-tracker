a58bedc815fe81855a282e3e6dac23b7f0cc18f6
Risgr8 <r2gupta@ucsd.edu>
2024-12-04 15:10:18 -0800

    finish creatine page

--- pages/creatine.py
+++ pages/creatine.py
@@ -2,19 +2,25 @@ import streamlit as st # type: ignore
 import streamlit_authenticator as stauth # type: ignore
 import yaml
 from yaml.loader import SafeLoader
+import datetime
 
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 init_creatine_amount = config["creatine"]["amount"]
-creatine_date = config["creatine"]["date"]
+creatine_date_refreshed = config["creatine"]["date_refreshed"]
+days_to_refresh = (datetime.date.today() - creatine_date_refreshed).days
+creatine_amount = max(0, init_creatine_amount-(5*days_to_refresh))
 
-creatine_amount = init_creatine_amount
-st.write("current creatine amount: " + creatine_amount)
 if st.button("add 5g"):
-    config["creatine"]["amount"] = creatine_amount + 5
+    creatine_amount = min(160, creatine_amount+5)
+    config["creatine"]["amount"] = creatine_amount
+    config["creatine"]["date_refreshed"] = datetime.date.today()
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file)
 if st.button("remove 5g"):
-    config["creatine"]["amount"] = creatine_amount + 5
+    creatine_amount = max(0, creatine_amount-5)
+    config["creatine"]["amount"] = creatine_amount
+    config["creatine"]["date_refreshed"] = datetime.date.today()
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file)
+st.write("current creatine amount: " + str(creatine_amount))
